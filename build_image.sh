60871c1cc81f9b73aba12f6a1d0267101bebbd51
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2021-12-13 08:44:25 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 rm -f build/Dockerfile
 mkdir build
 cp app.py build/.
+cd docker-artifact.mender build/.
+
 echo "FROM python:3.7-slim-buster" >> build/Dockerfile
 echo "RUN pip install paho-mqtt" >> build/Dockerfile
 echo "COPY app.py /app.py" >> build/Dockerfile
 echo "CMD python3 app.py" >> build/Dockerfile
+
 cd build
 docker stop tobias1172/amc_mqttclient
 docker rm tobias1172/amc_mqttclient
 docker build -t tobias1172/amc_mqttclient .
 docker push tobias1172/amc_mqttclient:latest
 docker images
+
+tar cfv docker-artifact.tar /build/docker-artifact.mender
