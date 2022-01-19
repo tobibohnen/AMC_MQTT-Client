6fa86a33235f1f6bc414aeca37f5cfcdcfce7844
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2022-01-19 20:02:22 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -7,6 +7,7 @@ cp docker-artifact.mender build/.
 
 echo "FROM --platform=linux/arm/v7 arm32v7/python:latest" >> build/Dockerfile
 
+echo "RUN pip install paho-mqtt" >> build/Dockerfile
 echo "COPY app.py /app.py" >> build/Dockerfile
 echo "CMD python3 app.py" >> build/Dockerfile
 
