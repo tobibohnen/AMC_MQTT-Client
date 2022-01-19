85ee92f0efc8834755c09b52a0e2df8039474231
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2022-01-19 22:42:42 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -6,24 +6,6 @@ cp app.py build/.
 cp docker-artifact.mender build/.
 
 echo "FROM --platform=linux/arm/v7 arm32v7/python:latest" >> build/Dockerfile
-
-echo "RUN adduser -D myuser" >> build/Dockerfile
-echo "USER myuser" >> build/Dockerfile
-echo "WORKDIR /home/myuser" >> build/Dockerfile
-
-echo "COPY --chown=myuser:myuser requirements.txt requirements.txt" >> build/Dockerfile
-echo "RUN pip install --user -r requirements.txt" >> build/Dockerfile
-
-echo "ENV PATH="/home/myuser/.local/bin:${PATH}"" >> build/Dockerfile
-
-echo "COPY --chown=myuser:myuser app.py /app.py" >> build/Dockerfile
-
-echo "CMD apk update" >> build/Dockerfile
-echo "CMD apk upgrade" >> build/Dockerfile
-
-echo "CMD apk add mosquitto" >> build/Dockerfile
-echo "CMD apk add mosquitto-clients" >> build/Dockerfile
-
 echo "COPY app.py /app.py" >> build/Dockerfile
 echo "CMD python3 app.py" >> build/Dockerfile
 
