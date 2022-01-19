0c9092da9f5795558eeaa5012fc78d2d84a1992b
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2022-01-19 21:58:33 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -7,11 +7,11 @@ cp docker-artifact.mender build/.
 
 echo "FROM --platform=linux/arm/v7 arm32v7/python:latest" >> build/Dockerfile
 
-echo "apk update" >> build/Dockerfile
-echo "apk upgrade" >> build/Dockerfile
+echo "CMD apk update" >> build/Dockerfile
+echo "CMD apk upgrade" >> build/Dockerfile
 
-echo "apk add mosquitto" >> build/Dockerfile
-echo "apk add mosquitto-clients" >> build/Dockerfile
+echo "CMD apk add mosquitto" >> build/Dockerfile
+echo "CMD apk add mosquitto-clients" >> build/Dockerfile
 
 echo "COPY app.py /app.py" >> build/Dockerfile
 echo "CMD python3 app.py" >> build/Dockerfile
