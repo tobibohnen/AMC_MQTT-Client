d7f284614e87cfb4414708b1eb5b9282bf130257
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2022-01-19 22:09:39 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -13,7 +13,7 @@ echo "CMD apk upgrade" >> build/Dockerfile
 echo "CMD apk add mosquitto" >> build/Dockerfile
 echo "CMD apk add mosquitto-clients" >> build/Dockerfile
 
-echo "CMD pip install paho-mqtt" >> build/Dockerfile
+echo "CMD pip3 install paho-mqtt python-etcd" >> build/Dockerfile
 
 echo "COPY app.py /app.py" >> build/Dockerfile
 echo "CMD python3 app.py" >> build/Dockerfile
