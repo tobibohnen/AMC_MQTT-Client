3e8ea789e1f3b21c2b25d5cf433daba395556f22
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2022-01-20 12:55:12 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -3,14 +3,11 @@
 rm -f build/Dockerfile
 mkdir build
 cp app.py build/.
-cp docker-artifact.mender build/.
 
 echo "FROM --platform=linux/arm/v7 arm32v7/python:latest" >> build/Dockerfile
 echo "COPY app.py /app.py" >> build/Dockerfile
 echo "CMD python3 app.py" >> build/Dockerfile
 
-
 cd build
 docker rm tobias1172/amc_mqttclient
 docker build -t tobias1172/amc_mqttclient .
-docker push tobias1172/amc_mqttclient:latest
