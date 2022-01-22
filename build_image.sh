a130e9db5dd621cf608c1e4d2ab8dba8d47e2d73
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2022-01-22 13:40:12 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -11,3 +11,4 @@ echo "CMD python3 app.py" >> build/Dockerfile
 cd build
 docker rm tobias1172/amc_mender_pipeline
 docker build -t tobias1172/amc_mender_pipeline .
+docker push tobias1172/amc_mender_pipeline:latest
