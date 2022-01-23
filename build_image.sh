1e68e8a2ff59e2f7feeed6c1842c9861569d0769
tobibohnen <61847079+tobibohnen@users.noreply.github.com>
2022-01-23 09:37:11 +0100

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-rm -f build/Dockerfile
-mkdir build
-cp app.py build/.
-
-echo "FROM --platform=linux/arm/v7 arm32v7/python:latest" >> build/Dockerfile
-echo "COPY app.py /app.py" >> build/Dockerfile
-echo "CMD python3 app.py" >> build/Dockerfile
-
-cd build
 docker rm tobias1172/amc_mender_pipeline
 docker build -t tobias1172/amc_mender_pipeline .
 docker push tobias1172/amc_mender_pipeline:latest
